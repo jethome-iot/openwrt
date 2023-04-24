2fdff3a0ff54f74b20443ea41bd1874433c220e0
lmahmutov <lmahmutov@wimark.com>
2023-04-24 15:58:11 +0300

    Allwinner: multiple fix. First working version

--- target/linux/allwinner/image/cortexa7.mk
+++ target/linux/allwinner/image/cortexa7.mk
@@ -7,13 +7,16 @@ define Device/mangopi_mq-r
   DEVICE_VENDOR := MangoPi
   DEVICE_MODEL := MQ-R
   SOC := sun8i-t113s
+  BLOCKSIZE := 128k
+  PAGESIZE := 2048
+  UBINIZE_OPTS := -E 5
   KERNEL_INSTALL := 0
   KERNEL_NAME := zImage
   KERNEL_SUFFIX := -zImage
   KERNEL := kernel-bin
   IMAGES := sysupgrade.bin dtb rootfs.bin
   IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
-  IMAGE/rootfs.bin := append-rootfs
+  IMAGE/rootfs.bin := append-ubi
   IMAGE/dtb := install-dtb
 endef
 TARGET_DEVICES += mangopi_mq-r
