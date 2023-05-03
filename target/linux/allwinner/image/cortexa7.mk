1742a0298359992f8d5a89189a0f2a20da12557c
Vyacheslav Bocharov <adeep@lexina.in>
2023-05-03 12:39:00 +0300

    Add new board: NANO-LV-LCD

--- target/linux/allwinner/image/cortexa7.mk
+++ target/linux/allwinner/image/cortexa7.mk
@@ -23,3 +23,23 @@ define Device/mangopi_mq-r
   IMAGE/xfel.bin := append-dtb | pad-to $$(DTS_SIZE) | append-kernel | pad-to $$(KERNEL_SIZE) | append-ubi
 endef
 TARGET_DEVICES += mangopi_mq-r
+
+define Device/sk_nano-lv
+  DEVICE_VENDOR := StarterKit
+  DEVICE_MODEL := NANO-LV-LCD
+  SOC := sun8i-t113s
+  DEVICE_DTS := sun8i-t113s-sk-nano-lv-nand
+  BLOCKSIZE := 128k
+  PAGESIZE := 2048
+  UBINIZE_OPTS := -E 5
+  KERNEL_INSTALL := 0
+  KERNEL_NAME := zImage
+  KERNEL_SUFFIX := -zImage
+  KERNEL := kernel-bin
+  KERNEL_SIZE := 8650752
+  DTS_SIZE := 262144
+  IMAGES := sysupgrade.bin xfel.bin
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+  IMAGE/xfel.bin := append-dtb | pad-to $$(DTS_SIZE) | append-kernel | pad-to $$(KERNEL_SIZE) | append-ubi
+endef
+TARGET_DEVICES += sk_nano-lv
