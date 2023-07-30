006d13986c14e4b92eda2bc3415fb6160e6449a6
Viacheslav Bocharov <adeep@lexina.in>
2023-07-30 14:52:07 +0300

    First update to 6.4

--- target/linux/allwinner/image/cortexa7.mk
+++ target/linux/allwinner/image/cortexa7.mk
@@ -5,13 +5,14 @@
 define Device/mangopi_mq-r 
   DEVICE_VENDOR := MangoPi
   DEVICE_MODEL := MQ-R
-  DEVICE_DTS := sun8i-t113s-mangopi-mq-r
+  DEVICE_DTS := sun8i-t113s-mangopi-mq-r-t113
+#sun8i-t113s-mangopi-mq-r
 endef
 TARGET_DEVICES += mangopi_mq-r
 
-define Device/sk_nano-lv
-  DEVICE_VENDOR := StarterKit
-  DEVICE_MODEL := NANO-LV-LCD
-  DEVICE_DTS := sun8i-t113s-sk-nano-lv-nand
-endef
-TARGET_DEVICES += sk_nano-lv
+#define Device/sk_nano-lv
+#  DEVICE_VENDOR := StarterKit
+#  DEVICE_MODEL := NANO-LV-LCD
+#  DEVICE_DTS := sun8i-t113s-sk-nano-lv-nand
+#endef
+#TARGET_DEVICES += sk_nano-lv
---
+++ target/linux/allwinner6.3/cortexa7/target.mk
@@ -0,0 +1,9 @@
+# SPDX-License-Identifier: GPL-2.0-only
+#
+# Copyright (C) 2017 Hauke Mehrtens
+
+include $(TOPDIR)/rules.mk
+
+BOARDNAME:=Allwinner T113
+CPU_TYPE:=cortex-a7
+CPU_SUBTYPE:=neon-vfpv4
